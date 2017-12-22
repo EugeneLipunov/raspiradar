08e21a69db8979f7516fb2051c8c6483dd741d6b
Eugene Lipunov <eugene.lipunov@cbcline.com>
2017-12-22 16:37:45 +0900

    respiradar.c

--- raspiradar.c
+++ raspiradar.c
@@ -7,10 +7,45 @@
 #define DEVNAME "/dev/ttyUSB1"
 #define BAUDRATE 921600
  
-void rdcb (void * param, const unsigned char * buf, const unsigned int size)
-{
-	 printf("%u bytes\n", size);
-}
+void rdcb (void * param, const unsigned char * buf, const unsigned int siz)
+{	int h = (int) param;
+	unsigned int i, n, frame;
+	STATE state, oldstate = UNDEF;
+	if (buf == 0) return;
+	if (siz == 0) return;
+	bsend (h, (char *) buf, siz);	//	try to load all data into the backet
+	while ((state = bwait (h)) != oldstate)
+	{	double tmp_p, tmp_x, tmp_y;
+		__int16 tmp;
+		switch (state)
+		{	case NOSYNC: break;		
+			case HDRRDY: break;		
+			case DATRDY:
+				n = get_number_of_tlvs (h);
+				frame = get_frame_number (h);
+				for (i = 0; i < n; i++)
+				{	unsigned int j, m, q;
+					DSC001 * pdsc;
+					OBJ001 * pobj;
+					if (get_tlv_type (h, i) != 1) continue;
+					pdsc = (DSC001 *) get_tlv_data (h, i);
+					pobj = (OBJ001 *) ((unsigned char *) pdsc + sizeof (DSC001));
+					if (pdsc == 0) continue;
+					if (pobj == 0) continue;
+					m = pdsc->NumberOfObjects;
+					q = pdsc->Qformat;
+					if (m == 0) continue;
+					for (j = 0; j < m; j++)
+					{	tmp = pobj[j].PeakValue;
+						tmp_p = X2F (tmp, q);
+						tmp = pobj[j].x;
+						tmp_x = X2F (tmp, q);
+						tmp = pobj[j].y;
+						tmp_y = X2F (tmp, q);
+						printf ("%2.2u: peak %2.2f, x:%2.2f, y:%2.2f\n", j, tmp_p, tmp_x, tmp_y);}
+					break;}
+				break;}
+		oldstate = state;}}
  
 int main (int argc, char* argv [])
 {
